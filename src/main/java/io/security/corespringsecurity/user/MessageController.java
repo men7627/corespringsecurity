3b7a028d13aaa133e1947406bf9f49453975e2d0
hyogeon <men1219@hanmail.net>
2020-06-01 23:49:11 +0900

    feat: project init

---
+++ src/main/java/io/security/corespringsecurity/user/MessageController.java
@@ -0,0 +1,13 @@
+package io.security.corespringsecurity.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+
+    @GetMapping("/messages")
+    public String messages() {
+        return "user/messages";
+    }
+}
